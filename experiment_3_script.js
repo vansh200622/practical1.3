40caa44481fc821aa2ac61e94f3eecd84ad60e4f
vansh200622 <160756790+vansh200622@users.noreply.github.com>
2025-08-07 10:17:31 +0530

    Add files via upload

---
+++ experiment_3_script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('themeToggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode', toggle.checked);
+});
